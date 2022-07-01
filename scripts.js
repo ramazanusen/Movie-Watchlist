f0cb4fdfe45110ed11ace2824b74ab7c15670bd8
Ramazan Usen <ramazanusen@hotmail.com>
2022-07-01 05:09:52 +0300

    getting movies data and put them into innerHtml

--- scripts.js
+++ scripts.js
@@ -1,56 +1,61 @@
 
 const apiKey = ""
 const movieListEl = document.querySelector(".movie-list-section")
+const imageUrl = "https://image.tmdb.org/t/p/w200"
 
-function getMovieByTitle(moviteTitle, apiKey){
-    fetch("https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language=en-US&query="+moviteTitle+"&include_adult=false")
+function getMovieById(moviteTitle, apiKey){
+    fetch("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&query=" + moviteTitle + "&include_adult=false")
         .then(response => response.json())
         .then(data => {
             for(let i = 0; i < data.results.length; i++){
-                console.log(data.results[i])
-                movieListEl.innerHTML += `
-                    <div class="movie-tab">
+                getMovieTitle(data.results[i].id, apiKey)
+            }
+        })
+}
+
+function getMovieTitle(movieId, apiKey) {
+    fetch("https://api.themoviedb.org/3/movie/" + movieId +"?api_key=" + apiKey + "&language=en-US")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const movieGenre = []
+        const movieRating = data.vote_average
+
+        if(data.genres.length > 0){
+            for(let i = 0; i < data.genres.length; i++){
+                movieGenre.push(data.genres[i].name)
+            }
+        }
+        else {
+            movieGenre.push("No genre data")
+        }
+
+       
+        movieListEl.innerHTML += `
+            <div class="movie-tab">
                         <div class="movie-image-container">
-                            <img class="movie-image" src="images/blade-runner.jpg">
+                            <img class="movie-image" src="${imageUrl+data.poster_path}">
                         </div>
 
                         <div class="movie-info-container">
                             <div class="movie-title-container">
-                                <h5 class="movie-title">${data.results[i].original_title}</h5>
-                                <p class="star">⭐${data.results[i].vote_average}</p>
+                                <h5 class="movie-title">${data.original_title}</h5>
+                                <p class="star">⭐${movieRating}</p>
                             </div>
                             
                             <div class="movie-detail-info-container">
-                                <p>116 min</p>
-                                <p>Drama, Mystery, Sci-fi</p>
-                                <p>➕ Watchlist</p>
+                                <p>${data.runtime} min</p>
+                                <p>${movieGenre.join(", ")}</p>
+                                <p id="add-watchlist">➕ Watchlist</p> 
                             </div>
                             <p class="movie-description">
-                                A blade runner must pursue and terminate 
-                                four replicants who stole a ship in space, 
-                                and have returned to Earth to find
-                                their creator.
+                                ${data.overview}
                             </p>
                         </div>
 
                     </div>
-                `
-            }
-        })
-
-    //data üzerinden image backdrop_path(image), 
-    //title,
-    //description,
-    //puanı,
-    //film süresi,
-    //türü (drama, sci-fi vs),
-    //bu verileri çek
-
-    //bu data'dan gelen bilgileri global variable içlerine atıp en son dizmek daha temiz. ç
-    //çünkü fetch edilen url'de her bilgi olmayabiliyor.
+        `
+    })
 }
 
-
-
-
-getMovieByTitle("the matrix", apiKey)
+getMovieById("matrix", apiKey)
